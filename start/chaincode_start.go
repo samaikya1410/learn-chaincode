8984803c71962905c0bc4fe15ea3ba3bf4921594
Samaikya Akarapu <samaikya_akarapu@persistent.co.in>
2017-01-25 13:44:45 +0530

    new demo code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -19,6 +19,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -39,9 +40,27 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
+	if len(args) != 4{
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	var party, operation, user string
+	var balance int
+	var err error
+	party = args[0]
+	operation = args[1]
+	user = args[2]
+	balance, err = strconv.Atoi(args[3])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for balance")
+	}
+	err = stub.PutState(party, []byte(operation))
+    if err != nil {
+        return nil, err
+    }
+	err = stub.PutState(user, []byte(strconv.Itoa(balance)))
+    if err != nil {
+        return nil, err
+    }
 
 	return nil, nil
 }
@@ -52,8 +71,18 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
-		return t.Init(stub, "init", args)
-	}
+		return t.Init(stub, "init",args)
+	} 
+	if function == "read" {
+        return t.read(stub,args)
+    }
+	if function == "write" {
+        return t.write(stub, args)
+    }
+	if function == "transfer" {
+        return t.transfer(stub, args)
+    }
+	
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -64,11 +93,102 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
-	}
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+func (t *SimpleChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    fmt.Println("running transfer()")
+
+    if len(args) != 5 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+    }
+	var party, operation, user1, user2 string
+	var amount, balance1, balance2 int
+	var err error
+    party = args[0]                           
+    operation = args[1]
+	user1 = args[2]
+	user2 = args[3]
+	amount, err = strconv.Atoi(args[4])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for transfer")
+	}
+	bal1,err :=  stub.GetState(user1)
+	balance1, _ = strconv.Atoi(string(bal1))
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if balance1<amount{
+		return nil, errors.New("less balance")
+	}
+	bal2,err := stub.GetState(user2)
+	balance2, _ = strconv.Atoi(string(bal2))
+	balance1 = balance1-amount
+	balance2 = balance2+amount
+    err = stub.PutState(user1,[]byte(strconv.Itoa(balance1)))  //write the variable into the chaincode state
+    if err != nil {
+        return nil, err
+    }
+	err = stub.PutState(user2,[]byte(strconv.Itoa(balance2)))  //write the variable into the chaincode state
+    if err != nil {
+        return nil, err
+    }
+	err = stub.PutState(party,[]byte(operation))  //write the variable into the chaincode state
+    if err != nil {
+        return nil, err
+    }
+    return nil, nil
+}
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    
+    if len(args) != 4{
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+	var party, operation, user string
+	var balance int
+	var err error
+	party = args[0]
+	operation = args[1]
+	user = args[2]
+	balance, err = strconv.Atoi(args[3])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for balance")
+	}
+	err = stub.PutState(party, []byte(operation))
+    if err != nil {
+        return nil, err
+    }
+	err = stub.PutState(user, []byte(strconv.Itoa(balance)))
+    if err != nil {
+        return nil, err
+    }
+
+	return nil, nil
+}
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    
+    if len(args) != 3{
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+	var party, operation, user string
+	var err error
+	party = args[0]
+	operation = args[1]
+	user = args[2]
+	balance, err := stub.GetState(user)
+	if err != nil {
+		return nil, errors.New("Expecting integer value for balance")
+	}
+	err = stub.PutState(party, []byte(operation))
+    if err != nil {
+        return nil, err
+    }
+	err = stub.PutState(user, balance)
+    if err != nil {
+        return nil, err
+    }
+
+	return nil, nil
 }
